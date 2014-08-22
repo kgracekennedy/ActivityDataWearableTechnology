3ac6607252525046d38ce1a3ab81faade0a39beb
kgracekennedy <kgracekennedy@gmail.com>
2014-08-22 10:14:06 -0400

    Initial upload to local

---
+++ run_analysis.R
@@ -0,0 +1,62 @@
+#features are the variable names for test and train
+features=read.table("UCIHAR_Dataset/features.txt")
+f=features[,2]
+
+#TrueFalse Vector finding all labels with mean 
+#and SD (why do they use std?)
+extract=grep("mean|std",f)
+#length(extract)#=79
+
+
+#30 possible test sujects
+#subject associated with each datapoint
+subjecttest=read.table("UCIHAR_Dataset/test/subject_test.txt")
+colnames(subjecttest)="id"
+#activity
+activitytest=read.table("UCIHAR_Dataset/test/Y_test.txt")
+colnames(activitytest)="Activity_Labels"
+#each motion data vector taken
+testdata=read.table("UCIHAR_Dataset/test/X_test.txt")
+colnames(testdata)=f
+test=testdata[,extract]
+#dim(test)#=79
+
+#30 possible training subjects
+#subject associated with each datapoint
+subjecttrain=read.table("UCIHAR_Dataset/train/subject_train.txt")
+colnames(subjecttrain)="id"
+#activity
+activitytrain=read.table("UCIHAR_Dataset/train/Y_train.txt")
+colnames(activitytrain)="Activity_Labels"
+#each motion data vector taken
+traindata=read.table("UCIHAR_Dataset/train/X_train.txt")
+colnames(traindata)=f
+train=traindata[,extract]
+#dim(train)#=79
+
+#Merge data
+data1=subjecttest
+#dim(test) #=2947x561, used to figure out howm any variables
+data1[,2]=activitytest
+data1[,3:81]=test
+data2=subjecttrain
+#dim(train) #=7352x561, used to figure out howm any variables
+data2[,2]=activitytrain
+data2[,3:81]=train
+#When we merge, we don't want NA's all willy nilly
+#The test and train data are from distinct subjects
+#so essentially we want them concatonated
+library(plyr)
+data=arrange(merge(data1,data2,all=T),id)
+#dim(data)#=81
+#data[1:3,1:2]
+
+#Activity Labels key
+actlab=read.table("UCIHAR_Dataset/activity_labels.txt")
+al=actlab[,2]
+
+for (i in 1:6) {
+    data$Activity_Labels[data$Activiy_Labels==i]=al[i]
+}
+
+write.table(data,file="CleanActivity.txt",row.name=F)
